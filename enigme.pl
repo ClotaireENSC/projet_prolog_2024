c69d87391b843ff248659873744d4a7f98e38f7e
Clotaire MARDON <cmardon001@ensc.fr>
2024-11-21 12:22:56 +0100

    enigme fini a 90pourcent

--- enigme.pl
+++ enigme.pl
@@ -30,9 +30,10 @@
 % VO : vin d^orties
 % FV : flammes violettes
 
+
+
 % Résolution de l^énigme des potions
 
-% Définition des types possibles de potions
 potion(flamme_noire).
 potion(flamme_violette).
 potion(poison1).
@@ -41,11 +42,37 @@ potion(poison3).
 potion(vin_ortie1).
 potion(vin_ortie2).
 
+type(flamme_violette,potion_flamme_noire).
+type(flamme_noire,potion_flamme_violette).
+type(poison1,poison).
+type(poison2,poison).
+type(poison3,poison).
+type(vin_ortie1, vin).
+type(vin_ortie2, vin).
+
 vin(X):- X = vin_ortie1; X = vin_ortie2.
 poison(X):- X = poison1; X = poison2; X = poison3.
 flamme(X):- X = flamme_noire; X = flamme_violette.
 
-disposition([P1,P2,P3,P4,P5,P6,P7]):-
+
+% rule 0 : Toutes les potions sont uniques
+indice0(L):- permutation([flamme_noire, flamme_violette, poison1, vin_ortie1, vin_ortie2, poison2, poison3],L).
+
+
+% rule 1 : Les poisons sont a gauche des vins
+pas_de_poison_dans([]).
+pas_de_poison_dans([P|Q]):- \+ poison(P), pas_de_poison_dans(Q).
+indice1([]).
+indice1([P|Q]):- potion(P), ((vin(P), pas_de_poison_dans(Q), indice1(Q)); (\+ vin(P), indice1(Q))).
+
+% rule 2 : Les extremes sont differents et pas solution pour feu violet
+indice2([P1,_,_,_,_,_,P7]):- potion(P1), potion(P7), P1 \== P7, P1 \== flamme_violette, P7 \== flamme_violette.
+
+% rule 4 : les deuxiemes extremes ont le meme gout 
+meme_gout(X,Y):- (vin(X), vin(Y)); (poison(X), poison(Y)); (flamme(X), flamme(Y)).
+indice4([_,P2,_,_,_,P6,_]):- potion(P2), potion(P6), P2\== P6, meme_gout(P2,P6).
+
+dispositions([P1,P2,P3,P4,P5,P6,P7]):-
     indice0([P1,P2,P3,P4,P5,P6,P7]),
     indice1([P1,P2,P3,P4,P5,P6,P7]),
     indice2([P1,P2,P3,P4,P5,P6,P7]),
@@ -53,16 +80,12 @@ disposition([P1,P2,P3,P4,P5,P6,P7]):-
     indice4([P1,P2,P3,P4,P5,P6,P7]).
 %    indice5([P1,P2,P3,P4,P5,P6,P7])
 
-meme_gout(X,Y):- (vin(X), vin(Y)); (poison(X), poison(Y)); (flamme(X), flamme(Y)).
-
-pas_de_poison_dans([]).
-pas_de_poison_dans([P|Q]):- \+ poison(P), pas_de_poison_dans(Q).
-
-indice0(L):- permutation([flamme_noire, flamme_violette, poison1, vin_ortie1, vin_ortie2, poison2, poison3],L).
-
-indice1([]).
-indice1([P|Q]):- potion(P), ((vin(P), pas_de_poison_dans(Q), indice1(Q)); (\+ vin(P), indice1(Q))).
-
-indice2([P1,_,_,_,_,_,P7]):- potion(P1), potion(P7), P1 \== P7, P1 \== flamme_violette, P7 \== flamme_violette.
-
-indice4([_,P2,_,_,_,P6,_]):- potion(P2), potion(P6), P2\== P6, meme_gout(P2,P6).
+solution([T1,T2,T3,T4,T5,T6,T7]):-
+    type(P1,T1),
+    type(P2,T2),
+    type(P3,T3),
+    type(P4,T4),
+    type(P5,T5),
+    type(P6,T6),
+    type(P7,T7),
+    dispositions([P1,P2,P3,P4,P5,P6,P7]).
