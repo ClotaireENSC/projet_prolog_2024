1ee2e42cca1d4ec3e5855ed006b600212f9e76a7
MarcIA <mmoreeuw@ensc.fr>
2024-12-16 10:45:33 +0100

    puissance 4 modif marc

--- puissance_quatre.pl
+++ puissance_quatre.pl
@@ -20,11 +20,15 @@ grilleInit(Liste) :-
              [*, *, *, *, *, *]].
 
 showGrid(Grid) :-
-    transpose(Grid, Transposed),
     write('Grille de Puissance 4:'), nl,
-    write('   1 2 3 4 5 6 7'),nl,
-    printColumns(Transposed, 1).
-% Print columns
+    write(' 1 2 3 4 5 6 '),nl,
+    printGrid(Grid).
+
+printGrid([]).
+printGrid([Row|Rest]) :-
+    write(Row), nl,
+    printGrid(Rest).
+
 printColumns([], _).
 printColumns([Head|Queue], Index) :-
     
@@ -45,6 +49,62 @@ replace_at_index(Index, [Head|Tail], NewValue, [Head|NewTail]) :-
 replace_at_index(1, [_|Tail], NewValue, [NewValue|Tail]).
 
 
+% VERIFIER SI LE JOUEUR A GAGNER EN ALLANT VOIR 
+
+% Vérifier si une sous-liste de 4 éléments consécutifs est égale au joueur
+sublist(List, Index, Player) :-
+    sublist_helper(List, Index, Player, 4).
+
+sublist_helper([Player, Player, Player, Player | _], _, Player, 4).
+sublist_helper([_ | Tail], Index, Player, Count) :-
+    NewCount is Count - 1,
+    sublist_helper(Tail, Index, Player, NewCount).
+
+
+
+
+horizontal_gagnant(Grid, X, Y, Player) :-
+    nth1(X, Grid, Row),
+    sublist(Row, Y, Player).
+
+% Vérifier une colonne verticale
+vertical_gagnant(Grid, X, Y, Player) :-
+    transpose(Grid, Transposed),
+    nth1(Y, Transposed, Column),
+    sublist(Column, X, Player).
+
+% Vérifier une diagonale montante
+diagonale_montante_gagnant(Grid, X, Y, Player) :-
+    diagonale_montante(Grid, X, Y, Player).
+
+% Vérifier une diagonale descendante
+diagonale_descendante_gagnant(Grid, X, Y, Player) :-
+    diagonale_descendante(Grid, X, Y, Player).
+
+
+% Vérifier une diagonale descendante (haut à gauche -> bas à droite)
+diagonale_descendante(Grid, X, Y, Player) :-
+    X =< 3, Y =< 4,  % Vérifie que les indices permettent une diagonale de 4 éléments
+    nth1(X, Grid, Row1),
+    nth1(Y, Row1, Player), % Vérifier la première case
+    X1 is X + 1, Y1 is Y + 1,
+    nth1(X1, Grid, Row2), nth1(Y1, Row2, Player), % Vérifier la deuxième case
+    X2 is X + 2, Y2 is Y + 2,
+    nth1(X2, Grid, Row3), nth1(Y2, Row3, Player), % Vérifier la troisième case
+    X3 is X + 3, Y3 is Y + 3,
+    nth1(X3, Grid, Row4), nth1(Y3, Row4, Player). % Vérifier la quatrième case
+
+% Vérifier une diagonale montante (bas à gauche -> haut à droite)
+diagonale_montante(Grid, X, Y, Player) :-
+    X >= 4, Y =< 4,  % Vérifie que les indices permettent une diagonale de 4 éléments
+    nth1(X, Grid, Row1),
+    nth1(Y, Row1, Player), % Vérifier la première case
+    X1 is X - 1, Y1 is Y + 1,
+    nth1(X1, Grid, Row2), nth1(Y1, Row2, Player), % Vérifier la deuxième case
+    X2 is X - 2, Y2 is Y + 2,
+    nth1(X2, Grid, Row3), nth1(Y2, Row3, Player), % Vérifier la troisième case
+    X3 is X - 3, Y3 is Y + 3,
+    nth1(X3, Grid, Row4), nth1(Y3, Row4, Player). % Vérifier la quatrième case
 
 
 
